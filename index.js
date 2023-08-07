70f9d16ca56febc71cae97fb9efcbd3a4f3a1264
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-08-07 15:48:48 +0600

    delete a post

--- index.js
+++ index.js
@@ -49,6 +49,23 @@ async function run() {
       })
 
 
+      app.delete('/allToy/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await toyCollection.deleteOne(query);
+        res.send(result);
+      })
+
+
+      
+
+
+
+      
+
+   
+
+
 
 
 
